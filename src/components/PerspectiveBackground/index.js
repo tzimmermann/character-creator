01486d6d38aa97d59c196d4e33203100da989fc2
Thomas Zimmermann <thomas.zimmermann@signavio.com>
2022-06-24 10:49:33 +0200

    solving exercises 1-4

---
+++ src/components/PerspectiveBackground/index.js
@@ -0,0 +1,7 @@
+import styles from "./PerspectiveBackground.module.css";
+
+function PerspectiveBackground() {
+  return <div className={styles.perspectiveBackground}></div>;
+}
+
+export default PerspectiveBackground;
